c7e5207856cdf08f72eddf9ec45009e3d393256b
huymingg <pressbrazing@gmail.com>
2025-08-16 22:23:26 +0700

    [UPDATE-FORM] tạo form

---
+++ FrontEnd/Areas/Admin/Views/QuanLyGopY.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FrontEnd.Areas.Admin.Views
+{
+    public class QuanLyGopYModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
